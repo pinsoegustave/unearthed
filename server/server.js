782afdf2b3e08ddda91b6ad651c7342e6c17bc5e
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-06-17 19:29:21 -0400

    install cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import giftsRouter from './routes/gifts.js'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
+
+app.use('/gifts', giftsRouter);
 
 app.get('/', (req, res) => {
     res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">UnEarthed API</h1>')
