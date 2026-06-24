3a0c33511a9bad024df51d3e6e3cf4f4812471ab
pinsoegustave <ntwalimudasubirag@gmail.com>
2026-06-24 19:29:12 -0400

    checkpoint 0: create controllers and routes

--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -9,5 +9,10 @@ router.get('/', GiftsControllers.getGifts)
 
 router.get('/:giftId', GiftsControllers.getGiftById);
 
+router.post('/', GiftsControllers.createGift);
+
+router.delete('/:id', GiftsControllers.deleteGift);
+
+router.patch(':id', GiftsControllers.updateGift);
 
 export default router
